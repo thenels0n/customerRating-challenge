6c984dbf85db37465034a68a018d07f6e42c32d6
Emmanuel Ademuyiwa <ademuyiwaemmanuel07@gmail.com>
2022-04-13 01:56:06 +0100

    first commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,11 @@ import React from 'react'
 
 const Button = (props) => {
   return (
-    <button onClick={() => props.onClick (props.data.id)} className={`btn ${ props.data.active ? 'active' : ''}`}>{props.data.rate}</button>
+    <button 
+      onClick={() => props.onClick (props.data.id)} 
+      className={`btn ${ props.data.active ? 'active' : ''}`}>{props.data.rate}
+    </button>
   )
 }
 
-export default Button
+export default Button;
